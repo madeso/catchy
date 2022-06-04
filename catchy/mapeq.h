bc9f0c7f251c040a61cc1f383eba3f5046181b02
Gustav <sir.gustav.the.coder@gmail.com>
2022-06-04 18:05:42 +0200

    feat: added unordered map to equality check

--- catchy/mapeq.h
+++ catchy/mapeq.h
@@ -2,6 +2,7 @@
 #define CATCHY_MAP_EQUALS_H
 
 #include <map>
+#include <unordered_map>
 #include <vector>
 #include <string>
 #include <sstream>
@@ -58,6 +59,54 @@ MapEq(const std::map<K, V>& lhs, const std::map<K, V>& rhs, const C& compare)
         ss << s;
     }
     
+    return catchy::FalseString::False(ss.str());
+}
+
+
+
+template<typename K, typename V, typename C>
+FalseString
+MapEq(const std::unordered_map<K, V>& lhs, const std::unordered_map<K, V>& rhs, const C& compare)
+{
+    std::vector<std::string> issues;
+
+    for (const auto& lhsp : lhs)
+    {
+        auto found = rhs.find(lhsp.first);
+        if (found == rhs.end())
+        {
+            issues.emplace_back(fmt::format("{} missing in rhs", lhsp.first));
+        }
+        else
+        {
+            const catchy::FalseString result = compare(lhsp.second, found->second);
+            if (result == false)
+            {
+                issues.emplace_back(fmt::format("{} was different: {}", lhsp.first, result.reason));
+            }
+        }
+    }
+
+    for (const auto& rhsp : rhs)
+    {
+        auto found = lhs.find(rhsp.first);
+        if (found == lhs.end())
+        {
+            issues.emplace_back(fmt::format("{} missing in lhs", rhsp.first));
+        }
+    }
+
+    if (issues.empty()) { return FalseString::True(); }
+
+    std::ostringstream ss;
+    bool first = true;
+    for (const auto& s : issues)
+    {
+        if (first) first = false;
+        else ss << "\n";
+        ss << s;
+    }
+
     return catchy::FalseString::False(ss.str());
 }
 
